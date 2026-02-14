30ab32052b036feeeacbcbca34e276e90f543613
Juzz22-bit <kornyushenko53@gmail.com>
2026-02-14 11:19:09 +0300

    Add files via upload

--- NEKE/JS/index.js
+++ NEKE/JS/index.js
@@ -1,6 +1,7 @@
 import HeaderFixed from "./header.js";
 import BurgerMenu from "./burger.js";
-import { sizesSlider } from "./sizes-slider.js";
+import { productSlider } from "./product-slider.js";
+import { sizes } from "./sizes.js";
 
 try {
     const headerFixed = new HeaderFixed({
@@ -28,7 +29,8 @@ try {
     );
 
     
-    sizesSlider();
+    productSlider();
+    sizes();
 } catch (error) {
     console.error(error);
 }
